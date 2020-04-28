e4ad5ed8e09d6185e29cdbf5c8673cc380c6e8f5
Claudius Daniel <mofehintoluwaclaudius@gmail.com>
2020-04-28 18:44:03 -0400

    fix for package socket.io

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
-var express = require('express'),
-    app = express(),
-    server = require('https').createServer(app),
-    io = require('socket.io').listen(server);
+var express = require("express"),
+  app = express(),
+  server = require("http").createServer(app),
+  io = require("socket.io").listen(server);
 
+//port
 var port = process.env.PORT || 3000;
 app.listen(port);
-console.log('Server running...' + port)
+console.log("Server running..." + port);
 
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html')
+//page routing
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
 
+//connection to websocket
+io.sockets.on('connection', function (socket) {
+  console.log('Socket Connected...');
+
+  // Send Message
+  socket.on('send message', function (data) {
+    io.sockets.emit('new message', {
+      msg: data
+    });
+  });
 });
