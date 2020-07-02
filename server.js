c45f049f7d09c334c8047e9c578f9ca88e9096d2
Mo Claudius <moclaudius@Mos-MacBook-Pro.local>
2020-07-02 07:37:14 -0400

    adding usernames

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var app = require("express")();
 var server = require("http").createServer(app),
   io = require("socket.io").listen(server);
 
+  usernames = [];
+
 //port
 var port = process.env.PORT || 3000;
 server.listen(port);
@@ -16,11 +18,37 @@ app.get("/", function (req, res) {
 //connection to websocket
 io.sockets.on('connection', function (socket) {
   console.log('Socket Connected...');
-
+  //add username
+  socket.on('add user', function (data, callback) {
+    if (usernames.indexOf(data) != -1) {
+      callback(false)
+    }
+    else {
+      callback(true)
+      socket.username = data
+      usernames.push(socket.username)
+      updateUsernames()
+    }
+    
+    //function to update usernames 
+    // this function is responsible for displaying usernames on the sidebar
+    function updateUsernames() {
+      io.sockets.emit('usernames', usernames)
+    }
+  });
   // Send Message
   socket.on('send message', function (data) {
     io.sockets.emit('new message', {
       msg: data
     });
+  
   });
+  // function to handle disconnect event
+  socket.on('disconnect', function (data) {
+    if (!socket.username) {
+      return;
+    }
+    usernames.splice(usernames.indexOf(socket.username), 1);
+    updateUsernames();
+  }); 
 });
